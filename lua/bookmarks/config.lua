6d0eda1ad328d16242c58e122e940093099d6128
jianglong <jianglong199212@gmail.com>
2023-06-29 09:34:27 +0800

    Revert "Update config.lua"
    This reverts commit c32e54beec52bf5ae7dfeb000e1f5fda7e60548f.

--- lua/bookmarks/config.lua
+++ lua/bookmarks/config.lua
@@ -4,7 +4,7 @@ local M = {
 
 function M.setup(user_config)
     M.data = {
-        mappings_enabled = false,
+        mappings_enabled = true,
         keymap = {
             toggle = "<tab><tab>", -- toggle bookmarks
             add = "\\z", -- add bookmarks
