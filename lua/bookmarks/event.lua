f77797a557d0917f1756cd24580dded233487850
jianglong <jianglong199212@gmail.com>
2023-07-26 11:37:59 +0800

    Update

--- lua/bookmarks/event.lua
+++ lua/bookmarks/event.lua
@@ -16,7 +16,8 @@ end
 function M.key_bind()
     vim.keymap.set("n", config.keymap.add, ":lua require'bookmarks'.add_bookmarks()<cr>", { silent = true })
     vim.keymap.set("n", config.keymap.toggle, ":lua require'bookmarks'.toggle_bookmarks()<cr>", { silent = true })
-    vim.keymap.set("n", config.keymap.delete_on_virt, ":lua require'bookmarks.list'.delete_on_virt()<cr>", { silent = true })
+    vim.keymap.set("n", config.keymap.delete_on_virt, ":lua require'bookmarks.list'.delete_on_virt()<cr>",
+        { silent = true })
     vim.keymap.set("n", config.keymap.show_desc, ":lua require'bookmarks.list'.show_desc()<cr>", { silent = true })
 end
 
@@ -29,9 +30,9 @@ function M.autocmd()
         callback = function()
             if config.fix_enable then
                 require("bookmarks.fix").fix_bookmarks()
+                local buf = api.nvim_get_current_buf()
+                m.set_marks(buf, l.get_buf_bookmark_lines(buf))
             end
-            local buf = api.nvim_get_current_buf()
-            m.set_marks(buf, l.get_buf_bookmark_lines(buf))
         end
     })
 
