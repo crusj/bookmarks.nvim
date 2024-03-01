33e4465914221563b9df83b4c502ff2335ab090b
jianglong <jianglong199212@gmail.com>
2024-03-01 10:48:08 +0800

    hl

--- lua/bookmarks/window.lua
+++ lua/bookmarks/window.lua
@@ -497,7 +497,7 @@ function M.preview_bookmark(filename, lineNumber)
             local cuts = filename:split_b(".")
             local ext = cuts[#cuts]
             if #cuts > 1 and ext ~= "" and data.bufp ~= nil then
-                api.nvim_buf_set_option(data.bufp, "syntax", ext)
+                api.nvim_buf_set_option(data.bufp, "filetype", "")
                 pcall(function()
                     vim.treesitter.start(data.bufp, ext)
                 end)
